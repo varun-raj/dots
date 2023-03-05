5ad20bf829563cded1783def2917dfdc3809bdb8
Varun Raj <varun@skcript.com>
2023-03-05 18:07:25 +0530

    fix
    Signed-off-by: Varun Raj <varun@skcript.com>

--- pages/display.tsx
+++ pages/display.tsx
@@ -1,10 +1,17 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import POVDisplay from '../components/POVDisplay'
 
 export default function DisplayPage() {
+  const router = useRouter()
+  // @ts-ignore
+  const delay = router.query.delay as number
   return (
-    <div className='bg-black min-h-full  flex flex-col items-center justify-center '>
-      <POVDisplay word="NITHRAN" delay={100} />
+    <div className='bg-black min-h-full flex flex-col items-center justify-center'>
+      <span className='text-center text-gray-800'>Delay: {delay}</span>
+      <div className='bg-black min-h-full flex flex-col items-center justify-center'>
+        <POVDisplay word="NITHRAN" delay={delay} />
+      </div>
     </div>
   )
 }

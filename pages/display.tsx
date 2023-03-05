251879b38480a74113252084109a39254c804f3e
Varun Raj <varun@skcript.com>
2023-03-05 18:22:02 +0530

    fix
    Signed-off-by: Varun Raj <varun@skcript.com>

--- pages/display.tsx
+++ pages/display.tsx
@@ -6,11 +6,12 @@ export default function DisplayPage() {
   const router = useRouter()
   // @ts-ignore
   const delay = router.query.delay as number
+  const word = router.query.word as string || 'N'
   return (
     <div className='bg-black min-h-full flex flex-col items-center justify-center'>
       <span className='text-center text-gray-800'>Delay: {delay}</span>
       <div className='bg-black min-h-full flex flex-col items-center justify-center'>
-        <POVDisplay word="NITHRAN" delay={delay} />
+        <POVDisplay word={word} delay={delay} />
       </div>
     </div>
   )
